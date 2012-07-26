6cc2cad0975da2bceaa75e30d1dda0901af07ad6
rknLA <kevin@rkn.la>
2012-07-26 00:26:03 -0700

    Generic rails app with some gemfile tweaks.

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.0'
+
+gem 'sqlite3'
+#gem 'pg' # for production
+
+gem 'devise'
+
+gem 'haml'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development do
+  gem 'pry'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'capybara-webkit'
+  gem 'pry'
+  gem 'fabrication'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GenericApp::Application.config.secret_token = 'c93a1cbe6e945d56a698439693d87bafc51cf77fd93f9197d0675901b3732eabbc2943dba087493148780389e400bd4e596706db5c6f7d7dbdc4f6bbb70a3e46'
